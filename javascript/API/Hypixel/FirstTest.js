69e9f646d90213b30fc0fe148b2802bc30c97f03
Marc Bernard <marc@bernard.mobi>
2021-05-14 12:28:18 +0200

    test

--- javascript/API/Hypixel/FirstTest.js
+++ javascript/API/Hypixel/FirstTest.js
@@ -2,14 +2,13 @@ const fetch = require("node-fetch");
 let file = require("../../../../../Keys/Hypixelapi.json");
 
 let key = file["key"];
-let name = "Mister_Iron";
-
-console.log(key);
+let name = "Goupil2000";
 
 fetch(`https://api.hypixel.net/player?key=${key}&name=${name}`)
 	.then((response) => response.json())
 	.then((data) => {
-		console.log(data);
+		let uuid = data.player.uuid;
+		console.log(uuid);
 	})
 	.catch((err) => {
 		console.log("Network Error", err);
